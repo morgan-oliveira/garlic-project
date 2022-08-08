0b8dac6424ad26606951c8ec2c1adf0b8a26ff7e
Morgan Oliveira <morgan.matprof@gmail.com>
2022-08-08 12:52:36 -0300

    read changelog

--- garlic.py
+++ garlic.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import time
+#import time
 
 window = tk.Tk()
 
@@ -7,30 +7,49 @@ mainFrame = tk.Frame(master = window).pack()
 
 window.title("Cadastro no Sistema")
 #window.geometry("320x240")
-name = tk.Label(text="Digite seu nome:").pack()
-enterName = tk.Entry().pack()
-password = tk.Label(text="Digite sua senha:").pack()
-enterPassword = tk.Entry().pack()
+getName = tk.StringVar()
+getPass = tk.StringVar()
 
-def validate(loginSucess):
+nameLabel = tk.Label(text="Digite seu nome:").pack()
+enterName = tk.Entry(textvariable=getName, width=16)
+enterName.pack()
+passwordLabel = tk.Label(text="Digite sua senha:").pack()
+enterPassword = tk.Entry(textvariable=getPass, show="*", width=16)
+enterPassword.pack()
+
+
+# validates username and password for admin users
+
+def validate():
+    name = enterName.get()
+    password = enterPassword.get()
+    if (name == "admin" and password == "nimda"):
+        return True
+    else: 
+        return False
+ 
+# creates the login window and calls 'validate' function to check if login is successful
     
-    #if loginSucess == True: 
-     #   print("works")
-    #else:
-     #   print("works even better")
-        
-def loginWindow (): 
-    loginSucessWindow = tk.Tk()
-    loginSucessWindow.title("Login")
-    loginFrame = tk.Frame(master = loginSucessWindow).pack()
-    time.sleep(3)
-    loginSucessMessage = tk.Label(loginSucessWindow, text="Login realizado com sucesso!").pack()
-    validate(False)
-    window.destroy()
-    return None      
+def loginWindowValidate(): 
+    boolvar = validate()
+    loginWindow = tk.Tk()
+    loginWindow.title("Login")
+    loginFrame = tk.Frame(master = loginWindow).pack()
+    # time.sleep(3)
+    if (boolvar == True):
+        window.destroy()
+        loginSucessMessage = tk.Label(loginWindow, text="Login realizado com sucesso!").pack()
+        loginSucessButton = tk.Button(loginWindow, text="OK", command=loginWindow.destroy).pack()
+        loginSucess()
+    else:         
+        loginFail = tk.Label(loginWindow, text="Usuário/Senha Incorreto!").pack()
+        loginSucessButton = tk.Button(loginWindow, text="OK", command=loginWindow.destroy).pack()        
+    return None
 
 button1 = tk.Button(
     text="Enter",
     fg="blue",
-    command=loginWindow
+    command=loginWindowValidate
     ).pack()
+
+window.mainloop()
